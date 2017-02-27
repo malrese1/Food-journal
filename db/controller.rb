95428d964bb6476e2df575a5445cbe4b0cb694fd
Malcom Johnson <malcomjohnson@malcoms-MacBook-Pro.local>
2017-02-27 14:19:03 -0500

    2nd commit

---
+++ db/controller.rb
@@ -0,0 +1,5 @@
+ActiveRecord::Base.establish_connection(
+  :adapter => "postgresql",
+  :database => "food-journal_development"
+  :database => "food-journal_test"
+)
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Post.destroy_all
+Comment.destroy_all
+
+posts = Post.create([
+    {title: "Too Much Tuna", content: "This is too much tuna!"},
+    {title: "Look At This Photograph", content: "How aboot this photograph of Tim Horton's, eh?"},
+    {title: "Canadians Like Tuna", content: "We gave Nickelback too much tuna!"}
+])
+
+comments = Comment.create([
+  {content: "Look at all that tuna!", post: posts[0]},
+  {content: "Your music is an embarrassment to Canadia!", post: posts[1]},
+  {content: "We're making a song aboot this tuna!", post: posts[2]}
+])
