a0d44e0964464ac45516bc2f59fbbef390db6416
Malcom Johnson <malcomjohnson@malcoms-MacBook-Pro.local>
2017-03-02 22:18:32 -0500

    6th commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,16 @@ Post.destroy_all
 Comment.destroy_all
 
 posts = Post.create([
-    {title: "Too Much Steak", content: "This is too much steak i dont think i can eat it all!"},
+    {title: "Monday PB&J!", content: "tems Needed: Sliced Bread, Peanut Butter, Jelly, Knife, Plate, Spoon Step 1: Obtain two slices of bread from the loaf of bread and set them down separately on your plate for later use. Pick up the jar of peanut butter in one hand and with the opposite, unscrew the top counter clock wise. After removing the top, set the jar and cap down on the top of the table. Now pick up the jelly jar and open the jar in the same manner as the peanut butter jar and after set the jelly jar and cap down on the table. Step 2: Grab hold of the open jar of peanut butter and pick up the knife with the free hand. Grab a portion of peanut butter with your knife. Put down the peanut butter. With the hand that is now available, pick up one slice of bread from the plate and spread the peanut butter evenly onto the slice of bread using a knife. After completion set down the slice of bread on the plate (peanut butter side face up) and the knife.
+
+Step 3: Pick up the jelly jar and the spoon with your free hand. Insert the spoon into the jelly jar and scoop out a decent portion of jelly. Set down the jelly jar and pick up the slice of bread with no peanut butter on it. Apply the jelly to the slice of bread and spread the jelly so that it is evenly distributed. After completion set the spoon down onto the plate. Step 4: While holding on to the slice of bread with jelly, pick up the slice of bread with peanut butter. Be sure to pick it up from the sides so that you do not stick your fingers. Adjust the slice of bread so that it is held on and open palm. Make sure that both slices of bread are facing the same direction. Bring both pieces of bread together as in a clapping motion."},
     {title:"2much2na",content: "Look at all that tuna!"},
     {title: "Special meal in D.C", content: "I love going to District Taco"},
     {title: "Canadians Like Tuna", content: "We gave Nickelback too much tuna!"}
 ])
 
 comments = Comment.create([
-  {content: "Look at all that tuna!", post: posts[0]},
+  {content: "I LOOOOOOOOOOVE PB&J", post: posts[0]},
   {content: "Chicken is better", post: posts[0]},
-  {content: "We're making a song aboot this tuna!", post: posts[0]}
+  {content: "We're making a song aboot this jelly!", post: posts[0]}
 ])
